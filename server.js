60559bcf98790b687cbfb0fcb46ed5012792f69d
Teryisam <101949970+Teryisam@users.noreply.github.com>
2025-07-28 04:36:40 +0100

    Update server.js
    Adding port

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ const createDatabase = async () => {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT || 3306,
     });
 
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\`;`);
